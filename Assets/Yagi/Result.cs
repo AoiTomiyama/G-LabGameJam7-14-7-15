155871445bf25edb6347050f2e6987b92a9aee6b
KRYG <abcdef0509a@gmail.com>
2024-07-14 15:38:20 +0900

    リザルトにスコアと番付を表示

---
+++ Assets/Yagi/Result.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour
+{
+    [SerializeField] Text _scoretext;
+    [SerializeField] Text _banduke;
+    void Start()
+    {
+        _scoretext.text = $"ëÃèd{PlayerPrefs.GetFloat("score")}ÉLÉç";
+        _banduke.text = $"î‘ït : {PlayerPrefs.GetString("ä÷òe")}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
